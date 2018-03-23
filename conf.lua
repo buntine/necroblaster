5cfa06a6bf1034eeb288d28e7475360023d31244
Andrew Buntine <info@bunts.io>
2018-03-23 18:10:31 +1100

    Window dimensions

---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+  t.title = "Necroblaster"
+  t.version = "0.10.2"
+  t.window.width = 700
+  t.window.height = 970
+end
--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
+function love.load(a)
+  love.graphics.setBackgroundColor(171, 205, 236)
+
+  sounds = {
+    song = love.audio.newSource("assets/sounds/mh_ritual.mp3")
+  }
+
+  sounds.song:setLooping(true)
+  love.audio.play(sounds.song)
+
+  
+end
+
 function love.draw()
+     love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
   love.graphics.print("Hello World", 400, 300)
 end
