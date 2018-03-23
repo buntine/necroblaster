926c9a4fcd3a1b515207c81a3e43fdc0881fa68c
Andrew Buntine <info@bunts.io>
2018-03-23 20:38:15 +1100

    Started reading JSON to runtime data structure

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
+json = require "lib/json"
+
 function love.load(a)
   love.graphics.setBackgroundColor(171, 205, 236)
+  love.graphics.setColor(0, 0, 0)
 
   sounds = {
     song = love.audio.newSource("assets/sounds/mh_ritual.mp3")
@@ -8,10 +11,17 @@ function love.load(a)
   sounds.song:setLooping(true)
   love.audio.play(sounds.song)
 
-  
+  local songFile = io.open("data/songs/mh_ritual.json", "r")
+  local songData = songFile:read("*a")
+  songFile:close()
+
+  keyMap = generateKeyMap(json.decode(songData))
 end
 
 function love.draw()
-     love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
-  love.graphics.print("Hello World", 400, 300)
+  love.graphics.print("Current FPS: "..tostring(love.timer.getFPS()), 10, 10)
+end
+
+function generateKeyMap(songData)
+  return songData
 end
