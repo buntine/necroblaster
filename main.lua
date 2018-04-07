cae9a63913bd0f0b9c4f534fa83f08dbebd043af
Andrew Buntine <info@bunts.io>
2018-04-07 21:53:56 +1000

    Show title

--- main.lua
+++ main.lua
@@ -8,11 +8,24 @@ require "laneways"
 require "railing"
 require "score"
 
-local play = {}
+local title = {}
 local menu = {}
+local play = {}
 
 function love.load()
   Gamestate.registerEvents()
+  Gamestate.switch(title)
+end
+
+function title:enter()
+  self.title = love.graphics.newImage("assets/images/title.png")
+end
+
+function title:draw()
+  love.graphics.draw(self.title, 0, 0)
+end
+
+function title:keypressed(key)
   Gamestate.switch(menu)
 end
 
@@ -31,63 +44,63 @@ function menu:keypressed(key)
 end
 
 function play:enter(_, songid)
-  bg = love.graphics.newImage("assets/images/background.png")
-  castle = love.graphics.newImage("assets/images/castle.png")
+  self.bg = love.graphics.newImage("assets/images/background.png")
+  self.castle = love.graphics.newImage("assets/images/castle.png")
 
-  song = Song:new(songid)
-  tapMap = TapMap:new(songid)
-  tapSet = TapSet:new()
-  laneways = LaneWays:new()
-  railing = Railing:new()
-  score = Score:new()
+  self.song = Song:new(songid)
+  self.tapMap = TapMap:new(songid)
+  self.tapSet = TapSet:new()
+  self.laneways = LaneWays:new()
+  self.railing = Railing:new()
+  self.score = Score:new()
 
-  song:play()
-  tapMap:generate()
+  self.song:play()
+  self.tapMap:generate()
 end
 
 function play:draw()
-  local frame = tapMap.framePointer
+  local frame = self.tapMap.framePointer
   local h = love.graphics.getHeight()
   local w = love.graphics.getWidth()
 
-  love.graphics.draw(bg, 0, 0)
+  love.graphics.draw(self.bg, 0, 0)
 
-  for _, l in pairs(laneways.lanes) do
+  for _, l in pairs(self.laneways.lanes) do
     l:render(w, h)
   end
 
-  score:render(tapSet.score)
-  railing:render(w, h)
+  self.score:render(self.tapSet.score)
+  self.railing:render(w, h)
 
-  love.graphics.draw(castle, 0, 0)
+  love.graphics.draw(self.castle, 0, 0)
 end
 
 function play:update()
   local h = love.graphics.getHeight()
-  local pos = song:tell()
+  local pos = self.song:tell()
 
-  tapMap:progress(pos)
+  self.tapMap:progress(pos)
 
-  for _, tap in ipairs(tapMap:futureTaps(pos)) do
-    if tap and not laneways:seen(tap) then
-      laneways:add(tap)
+  for _, tap in ipairs(self.tapMap:futureTaps(pos)) do
+    if tap and not self.laneways:seen(tap) then
+      self.laneways:add(tap)
     end
   end
 
-  if pos - railing.lastRail >= RAILING_FREQUENCY then
-    railing:add(pos)
+  if pos - self.railing.lastRail >= RAILING_FREQUENCY then
+    self.railing:add(pos)
   end
 
-  laneways:progress(h, tapMap.speed)
-  railing:progress(h, tapMap.speed)
-  score:progress(tapSet.score, tapMap.bestScore)
+  self.laneways:progress(h, self.tapMap.speed)
+  self.railing:progress(h, self.tapMap.speed)
+  self.score:progress(self.tapSet.score, self.tapMap.bestScore)
 end
 
 function play:keypressed(key, sc, ...)
-  for _, tap in ipairs(tapMap:currentTaps()) do
-    if tap and key == tap.char and not tapSet:seen(tap.id) then
-      tapSet:add(tap)
-      laneways.lanes[tap.char]:hit()
+  for _, tap in ipairs(self.tapMap:currentTaps()) do
+    if tap and key == tap.char and not self.tapSet:seen(tap.id) then
+      self.tapSet:add(tap)
+      self.laneways.lanes[tap.char]:hit()
     end
   end
 end
