46b27d0e1fa64e1fa025c13e6aceb0179b1d17c1
Andrew Buntine <info@bunts.io>
2018-04-22 22:18:37 +1000

    Merged result gamestate with Results object

--- gamestates/results.lua
+++ gamestates/results.lua
@@ -1,9 +1,10 @@
-require "result"
-
 results = {}
 
-function results:enter(_, score, bestScore)
-  self.result = Result:new(score, bestScore)
+function results:enter(_, s, bs)
+
+  self.score = s
+  self.bestScore = bs
+  self.percentage = round((s / bs) * 100)
 end
 
 function results:keypressed(_)
@@ -14,5 +15,23 @@ function results:draw()
   local w = love.graphics.getWidth()
   local h = love.graphics.getHeight()
 
-  self.result:render(w, h)
+  withColour(0.86, 0.11, 0.11, 1, function()
+    local score = love.graphics.newText(fonts.huge, self.percentage .. "%")
+    local x, y = unpack(center(w, h, score:getWidth(), score:getHeight(), 0, -100))
+
+    love.graphics.draw(score, x, y)
+  end)
+
+  withColour(0.78, 0.78, 0.78, 1, function()
+    local rank = love.graphics.newText(fonts.big, self:getRank())
+    local x, y = unpack(center(w, h, rank:getWidth(), rank:getHeight(), 0, 50))
+
+    love.graphics.draw(rank, x, y)
+  end)
+end
+
+function results:getRank()
+  local ranks = fun.filter(function (r) return r[1] > self.percentage end, RANKS)
+
+  return fun.nth(1, ranks)[2]
 end
