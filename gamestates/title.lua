09316e6921f43711dac50093016e0bed8849a4a0
Andrew Buntine <info@bunts.io>
2018-05-06 22:17:49 +1000

    Basic implementation of lightning in intro

--- gamestates/title.lua
+++ gamestates/title.lua
@@ -8,20 +8,20 @@ function title:enter()
   self.title = love.graphics.newImage("assets/images/title.png")
   self.music = love.audio.newSource("assets/audio/intro.ogg", "stream")
   self.rain = love.audio.newSource("assets/audio/rain.ogg", "stream")
+  self.thunder = love.audio.newSource("assets/audio/thunder.ogg", "stream")
+  self.lightning = -1
+  self.lightning1 = -1
   self.droplets = {}
   self.titleOpacity = 0.0
 
   self.music:play()
   self.rain:setVolume(0.1)
   self.rain:play()
+  self.thunder:setVolume(0.1)
 end
 
 function title:draw()
   scaleGraphics()
-
-  withOpacity(self.titleOpacity, function()
-    drawInCenter(self.title)
-  end)
 
   withoutScale(function()
     withColour(0.18, 0.18, 0.18, 1, function()
@@ -29,6 +29,22 @@ function title:draw()
         d:render()
       end
     end)
+
+    if self.lightning1 > 0 then
+      withColour(1, 1, 1, self.lightning1, function()
+        love.graphics.rectangle("fill", 0, 0, ACTUAL_WIDTH, ACTUAL_HEIGHT)
+      end)
+    end
+
+    if self.lightning > 0 then
+      withColour(1, 1, 1, self.lightning, function()
+        love.graphics.rectangle("fill", 0, 0, ACTUAL_WIDTH, ACTUAL_HEIGHT)
+      end)
+    end
+  end)
+
+  withOpacity(self.titleOpacity, function()
+    drawInCenter(self.title)
   end)
 
   self:drawTween()
@@ -47,6 +63,23 @@ function title:update()
 
   if self.titleOpacity < 1 then
     self.titleOpacity = self.titleOpacity + 0.001
+  end
+
+  if self.lightning > 0 then
+    self.lightning = self.lightning - 0.005
+  end
+
+  if self.lightning1 > 0 then
+    self.thunder:play()
+    self.lightning1 = self.lightning1 - 0.07
+  end
+
+  if self.titleOpacity > 0.97 and self.lightning1 == -1 then
+    self.lightning1 = 0.76
+  end
+
+  if self.titleOpacity > 0.99 and self.lightning == -1 then
+    self.lightning = 0.76
   end
 
   self:updateTween()
