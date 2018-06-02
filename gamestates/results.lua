a02434f2e8ba944d14e4e154dfef18cba8c06825
Andrew Buntine <info@bunts.io>
2018-06-02 21:18:37 +1000

    Added accuracy into results UI

--- gamestates/results.lua
+++ gamestates/results.lua
@@ -5,11 +5,13 @@ results = Transition()
 
 function results:enter(_, carry)
   local performance = round((carry.score / carry.bestScore) * 100)
-  local accuracy = round((carry.accuracy / carry.bestScore) * 100)
 
   self.layout = Layout("Results...", "menu_bg_church.png")
   self.score = love.graphics.newText(fonts.ridiculous, performance .. "%")
-  self.rank = love.graphics.newText(fonts.big, self:getRank(performance))
+  self.score_text = love.graphics.newText(fonts.big, "Performance")
+  self.accuracy_text = love.graphics.newText(fonts.big, "Accuracy")
+  self.accuracy = love.graphics.newText(fonts.ridiculous, carry.accuracy .. "%")
+  self.rank = love.graphics.newText(fonts.ridiculous, self:getRank(performance))
 end
 
 function results:keypressed(_)
@@ -20,9 +22,19 @@ function results:draw()
   scaleGraphics()
   self.layout:render()
 
+  local quarter = ACTUAL_WIDTH / 4
+
   withColour(0.66, 0.66, 0.66, 1, function()
-    drawInCenter(self.score, 0, -(self.score:getHeight() / 2) - 10)
+    drawInCenter(self.score, -quarter, -self.rank:getHeight())
+    drawInCenter(self.accuracy, quarter, -self.rank:getHeight())
     drawInCenter(self.rank, 0, self.rank:getHeight())
+  end)
+
+  withColour(0.44, 0.44, 0.44, 1, function()
+    local heading_offset = -self.rank:getHeight() + (self.score:getHeight() / 2) + 16
+
+    drawInCenter(self.score_text, -quarter, heading_offset)
+    drawInCenter(self.accuracy_text, quarter, heading_offset)
   end)
 
   self:drawTween()
